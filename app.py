f3b01ca35882df0f6bc3d03632e08c07d1cef992
Michel Atieno <mishqamish@gmail.com>
2022-01-27 10:44:19 +0300

    Not yet loading but reconfigured files

--- app.py
+++ app.py
@@ -6,11 +6,7 @@ import os
 #Init app
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def index():
-    return '<h1> Hello World </h1>'
 
-
-#Run Server
-if __name__ == '__main__':
-    app.run(debug = True)
+#Class/Model
+class User(db.Model):
+    phone_number = db.Column(db.Integer, primary_key=True)
--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,28 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from .config import config_options
+import os
 
-# Initializing application
-app = Flask(__name__)
 
-from app import views
+# basedir = os.path.abspath(os.path.dirname(__file__))
+
+#Init db
+db = SQLAlchemy()
+#Init ma
+ma = Marshmallow()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    #Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+  
+    db.init_app(app)
+    ma.init_app(app)
+
+    
+
+    return app
