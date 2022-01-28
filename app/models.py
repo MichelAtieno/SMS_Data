7cd89f5b78c773beea1f782a6d8bd582fbb970be
Michel Atieno <mishqamish@gmail.com>
2022-01-28 08:15:17 +0300

    Updated DB ready to test relationships

--- app/models.py
+++ app/models.py
@@ -1,9 +1,26 @@
 from . import db
+from datetime import datetime
 
 class User(db.Model):
-    _tablename__ = 'users'
-    phone_number = db.Column(db.Integer, primary_key=True)
+
+    id = db.Column(db.Integer, primary_key=True)
+    phone_number = db.Column(db.Integer, unique=True)
     username = db.Column(db.String(100))
+
+    transactions = db.relationship("Transaction", backref="account", lazy="True")
+  
 
     def __repr__(self):
         return f'User {self.username}'
+
+class Transaction(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Integer)
+    transacted = db.Column(db.DateTime,default=datetime.utcnow) 
+    category = db.Column(db.String(100), unique=True)
+
+    account_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+   
+    def __repr__(self):
+        return f"Transaction('{self.account_id}', '{self.amount}', '{self.category}')"
