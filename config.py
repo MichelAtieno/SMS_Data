1cbfaad1181e16950015263b3c601fcfd44bbb6f
Michel Atieno <mishqamish@gmail.com>
2022-01-31 13:46:09 +0300

    Edited Config.py

--- config.py
+++ config.py
@@ -14,8 +14,11 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://michel:mishy@localhost/sms_data'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    if ENV == 'dev':
+        SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://michel:mishy@localhost/sms_data'
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
+    else:
+        SQLALCHEMY_DATABASE_URI = 'postgres://issqhfagymjxlu:bd5008e19565cd3584f8235a963362e41174d600ce1efa177094d20179985ac2@ec2-3-216-113-109.compute-1.amazonaws.com:5432/d49etab5oknvkr'
 
 
 class DevConfig(Config):
