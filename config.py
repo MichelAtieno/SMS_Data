d0c496faf3858a2bc4ea1d7d69a0b687d766b58d
Michel Atieno <mishqamish@gmail.com>
2022-01-31 13:52:18 +0300

    Another error

--- config.py
+++ config.py
@@ -14,9 +14,9 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-        SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://michel:mishy@localhost/sms_data'
-        SQLALCHEMY_TRACK_MODIFICATIONS = False
-        DATABASE_URL = 'postgres://issqhfagymjxlu:bd5008e19565cd3584f8235a963362e41174d600ce1efa177094d20179985ac2@ec2-3-216-113-109.compute-1.amazonaws.com:5432/d49etab5oknvkr'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://michel:mishy@localhost/sms_data'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DATABASE_URL = 'postgres://issqhfagymjxlu:bd5008e19565cd3584f8235a963362e41174d600ce1efa177094d20179985ac2@ec2-3-216-113-109.compute-1.amazonaws.com:5432/d49etab5oknvkr'
 
 
 class DevConfig(Config):
